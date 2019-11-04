49277fddfde4be49b2c0fd85f53c6f803e548467
Ivan Ivaleev <ivaleev1997@gmail.com>
2019-11-04 22:48:07 +0300

    Created RecyclerView in MainActivity, TopTrackAdapter, added data binding and binding adapter with glide load image

--- app/src/main/java/com/lastfmlistmusic/MainActivity.kt
+++ app/src/main/java/com/lastfmlistmusic/MainActivity.kt
@@ -4,18 +4,33 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var mainActivityViewModel: MainActivityViewModel
+    lateinit var recyclerView: RecyclerView
+    lateinit var topTrackAdapter: TopTrackAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initView()
+    }
+
+    fun initView() {
         mainActivityViewModel = ViewModelProviders.of(this).get(MainActivityViewModel::class.java)
         mainActivityViewModel.tracksLiveData?.observe(this, Observer { t ->
-            println(t.size)
-         })
+            topTrackAdapter.listTopTracks = t
+            topTrackAdapter.notifyDataSetChanged()
+        })
+
+        recyclerView = findViewById(R.id.topTracksRecyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        topTrackAdapter = TopTrackAdapter()
+        recyclerView.adapter = topTrackAdapter
+
         mainActivityViewModel.loadData()
 
     }
